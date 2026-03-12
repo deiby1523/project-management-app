2a59d28bbcb0b1dca8f554ca26aac9ed40fc2f75
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2026-03-12 17:37:14 -0500

    User entity created

---
+++ backend/src/main/java/com/codemized/task_manager/model/User.java
@@ -0,0 +1,24 @@
+package com.codemized.task_manager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private Timestamp createdAt;
+}
